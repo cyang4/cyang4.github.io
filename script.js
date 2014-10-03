c30239cb12a37ca28e0341aba93e240362fb427f
cyang4 <cyang4@wellesley.edu>
2014-10-03 00:05:49 -0400

    Added date sorting option, print date submitted
    Still working on functionality for instantaneous re-sorting

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 var etag=null;
 
+	
+//other code
+
 function podURL() {
 	// temporary hack until we have a nice way for users to select their pod
 	//return "http://"+document.getElementById("username").value+".fakepods.com";
@@ -9,61 +12,73 @@ function podURL() {
 }
 
 
-function reload() {
+function reload(type) {
 
+	var t = type;
 	var request = new XMLHttpRequest();
 
 	// just fetch everything, for now, since queries don't work yet
 	request.open("GET", podURL()+"/_nearby", true);
 	if (etag !== null) {
 		request.setRequestHeader("Wait-For-None-Match", etag);
+		//console.log("In the if and state is "+request.readyState);
 	}
 
 	request.onreadystatechange = function() {
+		console.log("State is "+request.readyState);
 		if (request.readyState==4 && request.status==200) {
-    		handleResponse(request.responseText);
+    		handleResponse(request.responseText, t);
     	}
  	}
 
 	request.send();
 }
 
-function handleResponse(responseText) {
+function handleResponse(responseText, type) {
+	
 	var responseJSON = JSON.parse(responseText);
 	etag = responseJSON._etag;
 	var all = responseJSON._members;
 	var messages = [];
 	for (var i=0; i<all.length; i++) {
 		var item = all[i];
-		if ('profiles' in item) {
-			messages.push(item)
+		if ('profiles' in item ) {
+			if (item.profiles.name != "") //only want forms that are filled out
+				messages.push(item);
 		}
 	}
-	messages.sort(compareName);
-
+	if (type == "date"){
+		messages.sort(function(a,b){return a.time-b.time});
+		
+	}
+	else { //default is sort by name
+		messages.sort(compareName); //I arbitrarily chose to sort by name, this can change
+	}
+	//Sorting functions... more to come probably later
 	function compareName(a, b) {
-    if (a.profiles.name === b.profiles.name) {
-        return 0;
-    }
-    else {
-        return (a.profiles.name < b.profiles.name) ? -1 : 1;
-    }
-}
-	
+	    if (a.profiles.name === b.profiles.name) {
+	        return 0;
+	    }
+	    else {
+	        return (a.profiles.name < b.profiles.name) ? -1 : 1;
+	    }
+	}
+
 	var out = document.getElementById("profiles")
 	while(out.firstChild) { out.removeChild(out.firstChild) }
 	for (i=0; i<messages.length; i++) {
 		var message = messages[i];
-		
+		message.timeDate = new Date(Number(message.time));
 		var div = document.createElement("div");
 		div.innerHTML = "<p><b>Submitter: </b>"+message._owner+"<p><b>Name: </b>"+message.profiles.name
-							+"<p><b>Birthday: </b>"+message.profiles.birthday+"<p><b>Height: </b>"+message.profiles.height;
+							+"<p><b>Birthday: </b>"+message.profiles.birthday+"<p><b>Height: </b>"+message.profiles.height
+							+"<p><b>Date Submitted: </b>" + message.timeDate.toLocaleString();
 		out.appendChild(div);
 	}
 	document.getElementById("chat").style.visibility = "visible"
 	// wait for 100ms then reload when there's new data.  If data
 	// comes faster than that, we don't really want it.
-	setTimeout(reload, 50);
+	setTimeout(reload, 500);
 }
 
 
@@ -74,7 +89,7 @@ function newmsg() {
     
     var message = "<p>"+"<b>Name: </b>"+name+"<p>"+"<b>Birthday: </b>"+birthday+"<p>"+"<b>Height: </b>"+height;
 	document.getElementById("name").value = "";
-	document.getElementById("birthday").value="";
+	document.getElementById("birthday").value=""; //maybe in the future make sure this is in a valid format
 	document.getElementById("height").value="";
     if (message) {
      	var request = new XMLHttpRequest();
@@ -91,3 +106,4 @@ function newmsg() {
 		request.send(content);
 	} 
 }
+
