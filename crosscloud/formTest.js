b53f94a3f5411323273455e81b489dbb3dc30331
Cecille <cyang4@wellesley.edu>
2014-11-14 10:13:02 -0500

    Started working on auto-complete... Still messy and hard-coded

--- crosscloud/formTest.js
+++ crosscloud/formTest.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	processChange();
 });
 
-var hiddenFields = ["genreField", "colorField"];
+var hiddenFields = ["genreField", "colorField", "titleField", "bookTitleField"];
 
 function processChange(){
 	var t = document.getElementById("formType").value;
@@ -10,20 +10,23 @@ function processChange(){
 	if (t=="books"){
 		hideAll();
 		document.getElementById("genreField").style.display="block";
+		document.getElementById("bookTitleField").style.display="block";
 	}
 	else if (t=="electronics")
 	{
 		hideAll();
 		document.getElementById("colorField").style.display="block";
+		document.getElementById("titleField").style.display="block";
 	}
 	else{
 		hideAll();
+		document.getElementById("titleField").style.display="block";
 	}
 	
 }
 
 function hideAll(){
-	console.log("hiding all hidden fields");
+	//console.log("hiding all hidden fields");
 	for (i = 0; i<hiddenFields.length; i++){
 		document.getElementById(hiddenFields[i]).style.display="none";
 	}
@@ -34,3 +37,23 @@ function alertColor(){
 	var myElement = document.querySelector("#form-container");
 	//myElement.style.backgroundColor=c;
 }
+
+$(function() {
+
+    var availableTags = [
+      "Programming Interviews Exposed",
+      "Game of Thrones",
+      "The Wind-Up Bird Chronicle",
+      "Harry Potter and the Sorceror's Stone",
+      "Introduction to Algorithms",
+      "Harry Potter and the Chamber of Secrets",
+      "Harry Potter and the Prisoner of Azkaban",
+      "Harry Potter and the Goblet of Fire",
+      "Harry Potter and the Order of the Phoenix",
+      "Harry Potter and the Half Blood Prince",
+      "Harry Potter and the Deathly Hallows"
+    ];
+    $( "#bookFormTitle" ).autocomplete({
+      source: availableTags
+    });
+  });
