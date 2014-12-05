52043aea45771972767c8bc45fde5884ea60c935
Cecille <cyang4@wellesley.edu>
2014-12-05 02:11:36 -0500

    Image upload is working! Still on crosscloud 0.1.2

--- crosscloud/index3.js
+++ crosscloud/index3.js
@@ -1,24 +1,85 @@
 var pod = crosscloud.connect();
+
+    var conditions = {"1": "Like New",
+                        "2": "Excellent",
+                        "3": "Great",
+                        "4": "Good",
+                        "5": "Okay",
+                        "6": "Passable",
+                        "7": "Poor",
+                        "8": "Falling Apart",
+                        "9": "Junk",
+                        "10": "Other"};
+
+
+
 pod.onLogin(function (userID){
 
-	$("#products").html("waiting for data...");
+        //===============================================================================================
+        //
+        // Image upload stuff
+        //
+        //===============================================================================================
+        var dataURL;
+        var imagePreview = $('#imagePreview');
+        function handleFileSelect (evt) {
+            evt.preventDefault();
+            console.log("handling");
+            var files = evt.target.files; // FileList object
+
+            // Loop through the FileList and render image files as thumbnails.
+            for (var i = 0, f; f = files[i]; i++) {
+
+              // Only process image files.
+              if (!f.type.match('image.*')) {
+                console.log("is an image)");
+                continue;
+              }
+
+              var reader = new FileReader();
+
+              // Closure to capture the file information.
+              reader.onload = (function(theFile) {
+                console.log("reading image");
+                return function(e) {
+                  dataURL = e.target.result;
+                  // Render thumbnail.
+                  var thumbnail = ['<img src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+                  console.log(thumbnail);
+                  imagePreview.html(thumbnail);
+
+                };
+              })(f);
+
+              // Read in the image file as a data URL.
+              reader.readAsDataURL(f);
+            }
+          }
+
+        document.getElementById('imagePick').addEventListener('change', handleFileSelect, false);
+
+    $("#products").html("waiting for data...");
 
     pod.onLogout(function () {
         $("#products").html("<i>not connected</i>");
     });
+
+    //===============================================================================================
+    // displayMessages
+    // Create divs for each new item that we retrieve, called after we get all results on query
+    // Appends each new item to the end of the document
+    //===============================================================================================
+
     var displayMessages = function (messages) {
         
         messages.sort(function(a,b){return a.when<b.when?1:(a.when===b.when?0:-1)});
-        //var count = 0;
         var out = document.getElementById("products");
         out.innerHTML="";
         var i;
-        //alert(messages[1].description);
+        
         for (i=0; i<messages.length; i++) {
             var message = messages[i];
-            
-            //alert(message._id);
-            
+
             var itemType;
             if (message.isBooks){
                 itemType = "Books";   
@@ -37,59 +98,72 @@ pod.onLogin(function (userID){
         }
 
         if (Number(message.time) > 0) {
-            
+
+            var del = "delete";
             var div = document.createElement("div");
             div.className = "productInfo";
             message.timeDate = new Date(Number(message.time))
             var date = message.timeDate.toLocaleString();
-            
-            var line = "<div class='displayInfo' id='displayBrand'>"+message.brand+"</div>";
-            line += "<div>Price: "+message.price+"</div>";
-            line += "<div>Condition: "+conditions[message.condition]+"</div>";
-            line += "<div>Description: "+message.description+"</div>";
-            line += "<div>Type: "+itemType+"</div>";
-            line += "<div>Location: "+message.location+"</div>";
-            if (message.isBooks){line += "<div>Genre: " + message.genre+"</div>";}
+                
+            if(message.brand!=""){
+                var line = "<div class='displayInfo' id='displayBrand'><center>"+message.brand+"</center></div>";
+                if(message.dataURL!=null){line+='<div class = "imagePlace"><img src = "'+message.dataURL+'"/></div>';}
+                line += "<div>Price: "+message.price+"</div>";
+                line += "<div>Condition: "+conditions[message.condition]+"</div>";
+                line += "<div>Description: "+message.description+"</div>";
+                line += "<div>Type: "+itemType+"</div>";
+                line += "<div>Location: "+message.location+"</div>";
+                if (message.isBooks){line += "<div>Genre: " + message.genre+"</div>";}
+                
+                var link = document.createElement("a");
 
-            var link = document.createElement("a");
+                div.innerHTML += line;
+                link.href=message._id;
 
-            div.innerHTML = line;
-            link.href=message._id;
-            link.appendChild(document.createTextNode("item"));
-            div.appendChild(link);
-            
-            out.appendChild(div);
-            
+                link.appendChild(document.createTextNode("item"));
+                div.appendChild(link);
+                
+                out.appendChild(div);
+            }
         }
     }
-        /*if (count > show) {
-            $("#out").append("<p><i>("+(count-show)+" more not shown)</i></p>");
-        }*/
+        
     };
 
+
+
     pod.query()
-        .filter( { CrossCloudReuseList:true } )
+        .filter( { CrossCloudReuseList:true, isForSale3:true } )
         .onAllResults(displayMessages)
         .start();
 
-	
 
-	$(document).ready(function(){
-    	//toggle form visibility
-	    $('#forNewItem').click(function(){
-	        if(document.getElementById("form-container").style.display=="none")
-	            document.getElementById("form-container").style.display="block";
-	        else 
-	            document.getElementById("form-container").style.display="none";
-	        
-	    });
 
+    //===============================================================================================
+    // Processing for form
+    //===============================================================================================
+    $(document).ready(function(){
+        //toggle form visibility
+        $('#forNewItem').click(function(){
+            if(document.getElementById("form-container").style.display=="none")
+                document.getElementById("form-container").style.display="block";
+            else 
+                document.getElementById("form-container").style.display="none";
+            
+        });
+
+        
 
     processChange();
-	});
+    });
 
 
-	function makeJSON(brand, type, condition, description, price, location, genre, color){
+    //===============================================================================================
+    //
+    // Makes the actual JSON file.
+    //
+    //===============================================================================================
+    function makeJSON(brand, type, condition, description, price, location, genre, dataURL){
     var file = {};
     file["CrossCloudReuseList"] = true;
     file["brand"] = brand;
@@ -99,7 +173,7 @@ pod.onLogin(function (userID){
     //file["color"] = color;
     file["location"] = location;
     file["genre"] = genre;
-    file["color"] = color;
+    file["dataURL"] = dataURL;
     
     var isElectronics = (type == "electronics");
     var isClothing = (type == "clothing");
@@ -112,20 +186,25 @@ pod.onLogin(function (userID){
     file["isBooks"] = (type == "books");
     file["isFurniture"] = (type == "furniture");
     file["isOther"] = (type == "other");
-    file["isForSale"] =  true; //temporarily set this as true, since we don't have a 'looking for' feature
+    file["isForSale3"] =  true; //temporarily set this as true, since we don't have a 'looking for' feature
     file["time"] = Date.now();
-    
+    console.log(file);
     return file;
-	}
+    }
 
 
 
-	$(function(){
+    $(function(){
     $("#error").html("");  // clear the "Missing Javascript" error message
 
-    var pod = crosscloud.connect();
-    var myMessages = [];
 
+    //===============================================================================================
+    //
+    // sendProduct
+    // Function that is called when an item is submitted. Takes into account special variables
+    // if an item has different form parameters (electronics vs. books vs. other vs. clothing)
+    //
+    //===============================================================================================
     var sendProduct = function () {
         var genre = "";
         var color = "";
@@ -136,102 +215,74 @@ pod.onLogin(function (userID){
 
         if (type=="books"){var genre = document.getElementById("genre").value;}
         else {var genre = "";}
-        if (type =="electronics"){var color = document.getElementById("color").value;}
-        else{var color = "";}
+        
 
         var condition = document.getElementById("condition").value;
         var description = document.getElementById("description").value;
         var price = document.getElementById("price").value;
         var location = document.getElementById("location").value;
-      
-
-        var thisJSON = makeJSON(brand, type, condition, description, price, location, genre, color);
+        var image = dataURL;
+        var thisJSON = makeJSON(brand, type, condition, description, price, location, genre, image);
         console.log(thisJSON);
         var content = JSON.stringify(thisJSON);
-        
-        
-        myMessages.push(thisJSON);
-        pod.push(thisJSON);
+
+       pod.push(thisJSON);
 
         document.getElementById("formTitle").value = "";
         document.getElementById("bookFormTitle").value = "";
-        document.getElementById("formType").value = "";
+        //document.getElementById("formType").value = "";
         document.getElementById("genre").value = "";
         document.getElementById("color").value = "";
         document.getElementById("description").value = "";
-        document.getElementById("condition").value = "";
+        //document.getElementById("condition").value = "";
         document.getElementById("price").value = "";
         document.getElementById("location").value = "";
+
 
     };
 
     $("#submitbutton").click(sendProduct);
-
-    // allow the enter key to be a submit as well
-    /*$("#nick").keypress(function (e) {
-        if (e.which == 13) {
-            $("#helloButton").click();
-            return false;
-        }
-    });*/
-
-    //var show = 12;
-
-    var conditions = {"1": "Like New",
-                        "2": "Excellent",
-                        "3": "Great",
-                        "4": "Good",
-                        "5": "Okay",
-                        "6": "Passable",
-                        "7": "Poor",
-                        "8": "Falling Apart",
-                        "9": "Junk",
-                        "10": "Other"};
-
-
-    
-
+    //===============================================================================================
+    //
+    // Individual queries for the different categories. Should clean this up...
+    //
+    //===============================================================================================
 
     function qElec(){
     clearList();
-    console.log("refreshing Electronics");
     pod.query()
-            .filter( { isElectronics:true } )
+            .filter( { isElectronics:true , isForSale3:true} )
             .onAllResults(displayMessages)
             .start();
         }
-     function qBook(){
+    function qBook(){
     clearList();
-    console.log("refreshing");
     pod.query()
-            .filter( { isBooks:true } )
+            .filter( { isBooks:true, isForSale3:true} )
             .onAllResults(displayMessages)
             .start();
         }
 
-     function qClothing(){
+    function qClothing(){
     clearList();
-    console.log("refreshing");
     pod.query()
-            .filter( { isClothing:true } )
+            .filter( { isClothing:true, isForSale3:true } )
             .onAllResults(displayMessages)
             .start();
         }
 
     function qFurniture(){
     clearList();
-    console.log("refreshing");
     pod.query()
-            .filter( { isFurniture:true } )
+            .filter( { isFurniture:true, isForSale3:true } )
             .onAllResults(displayMessages)
             .start();
         }
 
-     function qOther(){
+    function qOther(){
     clearList();
-    console.log("refreshing");
     pod.query()
-            .filter( { isOther:true } )
+            .filter( { isOther:true , isForSale3:true} )
             .onAllResults(displayMessages)
             .start();
         }
@@ -241,17 +292,28 @@ pod.onLogin(function (userID){
     $('#furnitureLink').click(qFurniture);
     $('#otherLink').click(qOther);
 
+    //===============================================================================================
+    //
+    // clearList
+    // Removes all elements in the products div for each new query
+    //
+    //===============================================================================================
     function clearList(){
         var out = document.getElementById("products");
             out.innerHTML="";
     }
-
+});
 });
 
 
-});
 var hiddenFields = ["genreField", "colorField", "titleField", "bookTitleField"];
 
+//===============================================================================================
+//
+// processChange
+// Used when a different item type is selected for item submit form
+//
+//===============================================================================================
 
 function processChange(){
     var t = document.getElementById("formType").value;
@@ -274,6 +336,12 @@ function processChange(){
     
 }
 
+//===============================================================================================
+//
+// hideAll
+// Hides all fields that could be hidden, used when new item type is selected
+//
+//===============================================================================================
 function hideAll(){
     //console.log("hiding all hidden fields");
     for (i = 0; i<hiddenFields.length; i++){
@@ -281,9 +349,15 @@ function hideAll(){
     }
 }
 
-
+//===============================================================================================
+//
+// Freebase widget
+//
+//===============================================================================================
 $(function() {
   $("#bookFormTitle").suggest({"filter":"(all type:/book/book/)",
                                 "key": "AIzaSyA-dZJY_JIQjV6zMlEvmkyi-ymQInQzSwk",
                               "suggest_new": "Click on me if you don't see anything in the list"});
 });
+
+
